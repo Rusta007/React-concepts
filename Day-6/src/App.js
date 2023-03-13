239d0c0601ec41582e4b50ac0a1fef589fc8587c
Jyoti Prajapati <94469107+Rusta007@users.noreply.github.com>
2023-03-13 22:52:51 +0530

    Add files via upload

---
+++ Day-6/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import FetchApp from './Component/Fetch';
+import PromiseAPI from './Component/Promise';
+
+function App() {
+  return (
+    <div className="App">
+     <FetchApp/>
+      <PromiseAPI/>
+    </div>
+  );
+}
+
+export default App;
